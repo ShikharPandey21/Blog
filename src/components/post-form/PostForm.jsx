b81db69e0d0e6fe488e4a0953d46350e3e1dd6c7
Shikhar Pandey <pandeyshikhar007@gmail.com>
2026-02-17 12:31:38 +0530

    adding form&slugs

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -6,7 +6,40 @@ import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 
-function PostForm() {
+function PostForm({post}) {
+  const {register,handleSubmit,watch,setValue,
+  control,getValues} = useForm({
+    defaultValues:{
+      title : post?.title||'',
+      slug : post?.slug||'',
+      content : post?.content||'',
+      status : post?.status||'active',
+    },
+  })
+
+  const navigate = useNavigate()
+  const userData = useSelector(state => state.user.userData)
+
+  const submit = async(data)=>{
+    if(data){
+      const file = data.image[0]? appwriteService.uploadFile(data.image[0]): null
+    }
+
+    if(file){
+      appwriteService.deleteFile(post.featuredImage)
+    }
+
+    const dbPost = await appwriteService.updatePost(post.$id, {
+      ...data,
+      featuredImage: file? file.$id : undefined,
+
+      if(dbPost){
+        navigate(`/post/${dbPost.$id}`)
+      }
+    }
+  
+  )
+  }
   return (
     <div>Postform</div>
   )
